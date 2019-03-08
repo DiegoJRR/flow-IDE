7ddc04cdfe7001ab71fe58ee56dcc36dcc946caa
LulLz <efrainquinteronarvaez@Sastrecillo.local>
2019-03-07 20:21:25 -0600

    Carpeta de Widgets

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ from kivy.uix.tabbedpanel import TabbedPanelHeader
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.button import Button
 from kivy.core.window import Window
+from kivy.lang import Builder
+from os import listdir
 
 Window.size = (1200,800)
 
-class tabManager(TabbedPanel):
+kv_path = './widgets/'
+for kv in listdir(kv_path):
+	if(kv[-3:]==".kv"):
+		with open(kv_path+kv, encoding='utf8') as archivoWidget: 	#Se abre el archivo para poder leer acentos y caracteres UTF-8,
+			Builder.load_string(archivoWidget.read())				#si se abre con la herramienta de Kivy estos no se visualizan.
+
+class TabManager(TabbedPanel):
 	pass
 	
 class EditLayout(StackLayout):
@@ -20,7 +28,7 @@ class tab(TabbedPanelHeader):
 	pass
 
 class flowideApp(App):
-	layout=tabManager(do_default_tab=False)
+	layout=TabManager()
 	editLayout=EditLayout()
 	configLayout=ConfigLayout()
 	
