4b92a49dcc300550e16883dda534982face189cc
LulLz <efrainquinteronarvaez@Sastrecillo.local>
2019-03-27 17:43:22 -0600

    lines

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from kivy.app import App
 from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.tabbedpanel import TabbedPanelHeader
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.core.window import Window
 from kivy.lang import Builder
+from kivy.graphics import Color,Line,Rectangle
+from kivy.uix.widget import Widget
+
 from os import listdir
 
 Window.size = (1200,800)
@@ -18,9 +22,62 @@ for kv in listdir(kv_path):
 class TabManager(TabbedPanel):
 	pass
 	
-class EditLayout(BoxLayout):
+	
+class picker(Widget):
+	touched=False
+	line=None
+	points=[]
+	def __init__(self,**kwargs):
+		super(picker,self).__init__(**kwargs)
+		self.size_hint=(None,None)
+		self.bind(pos=self.update_canvas)
+		self.bind(size=self.update_canvas)
+		self.update_canvas()
+	def update_canvas(self,*args):
+		self.canvas.clear()
+		with self.canvas:
+			Color(0,0,1,1)
+			Rectangle(pos=self.pos,size=self.size)
+			
 	def on_touch_down(self,touch):
-		 print('The touch is at position', touch.pos)
+		if(self.collide_point(touch.pos[0], touch.pos[1])):
+			self.touched=True
+			with self.canvas:
+				self.points=[self.pos[0],self.pos[1],touch.pos[0],touch.pos[1]]
+				self.line=Line(points=self.points,width=2)
+	def on_touch_up(self,touch):
+			self.touched=False
+		
+	def on_touch_move(self,touch):
+		if(self.touched):
+			print("blu",touch.pos)
+			self.points[2]=touch.pos[0]
+			self.points[3]=touch.pos[1]
+			self.line.points=self.points
+	
+class box(Widget):
+	def __init__(self,**kwargs):
+		super(box,self).__init__(**kwargs)
+		self.size_hint=(None,None)
+		self.bind(pos=self.update_canvas)
+		self.bind(size=self.update_canvas)
+		self.update_canvas()
+		self.add_widget(picker(pos=self.pos,size=(20,10)))
+		
+	def update_canvas(self,*args):
+		self.canvas.clear()
+		with self.canvas:
+			Color(1,0,0,1)
+			Rectangle(pos=self.pos,size=self.size)
+
+class EditLayout(FloatLayout):
+	xd=True
+	def on_touch_down(self,touch):
+		if(self.xd):
+			if(self.collide_point(touch.pos[0], touch.pos[1])):
+				self.add_widget(box(pos=touch.pos,size=(30,30)))
+			self.xd=False
+		super(EditLayout, self).on_touch_down(touch) 
 	
 class ConfigLayout(BoxLayout):
 	pass
